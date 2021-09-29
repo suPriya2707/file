4a72e3a3377ee6f3dccf555938a8daafdef97ed1
Supriya <supriya.jarugu@gmail.com>
2021-09-29 06:19:25 +0000

    fst commit

--- Pro_03/src/com/Main.java
+++ Pro_03/src/com/Main.java
@@ -1,8 +1,49 @@
 package com;
 
+import java.util.Scanner;
+
 public class Main {
+
 	public static void main(String[] args) {
-		Welcome.printWelcome("hi", "Supriya");
+		
+		// Create "main" folder if not present in current folder structure
+		Fileop.createMainFolderIfNotPresent("main");
+
+		MenuOP.printWelcome("VitualPro", "Supriya");
+
+		handleWelcomeScreenInput();
+		HandleOperations.handleWelcomeScreenInput();
+	}
+
+	private static void handleWelcomeScreenInput() {
+		boolean running = true;
+		Scanner sc = new Scanner(System.in);
+		do {
+			try {
+				MenuOP.displayMenu();
+				int input = sc.nextInt();
+
+				switch (input) {
+				case 1:
+					Fileop.displayAllFiles("main");
+					break;
+				case 2:
+					MenuOP.handleFileMenuOptions();
+					break;
+				case 3:
+					System.out.println("Program exited successfully.");
+					running = false;
+					sc.close();
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Please select a valid option from above.");
+				}
+			} catch (Exception e) {
+				System.out.println(e.getClass().getName());
+				handleWelcomeScreenInput();
+			} 
+		} while (running == true);
 	}
 
 }
